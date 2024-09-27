07787c25de405d12ce67026fec915f5efe748075
Jeremy M. G. Leung <jml230@pitt.edu>
2024-09-27 14:11:21 -0400

    force legacy behavior in 1.26.*, else warn in numpy2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,6 @@
+from packaging.version import Version
 import numpy as np
 
-np._set_promotion_state("weak_and_warn")
+
+if Version(np.__version__) >= Version('2'):
+    np._set_promotion_state("weak_and_warn")
